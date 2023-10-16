1f4f1688c953b1d28d55893aa4025c8209056852
ivanizidoro <ivanizidoro@hotmail.com>
2023-10-16 20:05:14 -0300

    alteração no javascript

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ function generateNumber() {
     const min = Math.ceil(document.querySelector(".input-min").value)
     const max = Math.floor(document.querySelector(".input-max").value)
 
+    if(min >= max){
+        alert("O valor minino tem que ser MENOR que o valor maxino")
+    } else{
+        const result = Math.floor(Math.random() * (max - min + 1)) + min
+        alert(result)
+    }
     const result = Math.floor(Math.random() * (max - min + 1)) + min;
     alert(result)
 }
